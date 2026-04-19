e33fd7085d6e9cbc131c4f783986ff89f47c3012
HanifMuhammad-py <hanifmhmmd13@gmail.com>
2026-04-19 18:55:51 +0700

    add if and else testing.py

---
+++ IfandElse.py
@@ -0,0 +1,29 @@
+print("if and Else Statement")
+
+# if statement
+# if (==) kondisi : aksi
+
+# # if (basic)
+# nama = input("masukan nama anda : ")
+# if nama=="hanif" : print("selamat datang pak Hanif")
+# print(f"silahkan masuk pak {nama}")
+
+# # if (indentation)
+# nama = input("masukan nama anda : ")
+# if nama=="hanif" : 
+#     print("selamat datang pak Hanif")
+#     print("CEO & Founder dari Several Oracles")
+# print(f"silahkan masuk pak {nama}")
+
+# if & else & elif (indentation)
+nama = input("masukan nama anda : ")
+if nama=="hanif" :
+    print("Selamat datang pak hanif")
+    print("CEO & Founder dari Several Oracles")
+elif nama=="fixera" :
+    print("Selamat datang pak fixera")
+    print("Co-Founder dari Several Oracles")
+else :
+    print(f"selamat datang {nama}")
+    print("jangan lupa absen! kerja!")
+print("terima kasih")
