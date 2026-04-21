e769ab1417738770a33af937ca390015be7cb388
HanifMuhammad-py <hanifmhmmd13@gmail.com>
2026-04-21 07:23:10 +0700

    add While Loop test

---
+++ WhileLoop.py
@@ -0,0 +1,31 @@
+print("While Loop")
+
+# while kondisi :
+#     aksi A
+#     Aksi B
+
+# contoh 1
+angka = 0
+print(f'angka sekarang adalah = {angka}')
+while angka < 5 :
+    angka += 1 
+    print("true !")
+print("end\n")
+
+
+# contoh 2
+angka = 0
+print(f'angka sekarang adalah = {angka}')
+while angka > -5 :
+    angka -= 1 
+    print("true !")
+print("end\n")
+
+
+# contoh 3
+angka = 2 + 7
+print(f'angka sekarang adalah = {angka}')
+while angka > 5 :
+    angka -= 1 
+    print("true !")
+print("end\n")
