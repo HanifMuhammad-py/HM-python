53821248a6ae9d30ef3d3a268d0db0531a85f0b6
HanifMuhammad-py <hanifmhmmd13@gmail.com>
2026-04-26 21:19:39 +0700

    add Dictionary Function testing

---
+++ Dictionary.py
@@ -0,0 +1,21 @@
+print("Dictionary\n")
+
+# Normal dat
+data = ["Hanif", "raja", "python"]
+print(data[2])
+
+# Dictionary (dict) = Associative array
+# <variable {}
+# identifier : key
+# how to print ? print(<variable>[variable dict])
+
+data_dict = {
+    "x"     : "1312", 
+    "y"     : "Several",
+    "z"     : "Oracles",
+    "fx"    : [13, 12, 2],
+    "svor"  : 1312 
+}
+print(f'{data_dict["x"]}{data_dict["fx"]}')
+print(f'{data_dict["y"]} {data_dict["z"]}')
+print(f'data pada dict svor --> {data_dict["svor"]}')
