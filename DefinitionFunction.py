9e2002131578c780942c2c668fb5a2a189751fa6
HanifMuhammad-py <hanifmhmmd13@gmail.com>
2026-05-02 17:13:39 +0700

    add Def Function testing

---
+++ DefinitionFunction.py
@@ -0,0 +1,31 @@
+print("Definition Function \n")
+
+# def <variable>() :
+# def = definition
+
+def hello_world() :
+    print("hello_world")
+    print("h")
+    print("e")
+    print("l")
+    print("l")
+    print("o")
+    print("w")
+    print("o")
+    print("r")
+    print("l")
+    print("d")
+pass
+
+hello_world()
+
+
+def function_x() :
+    a = 5
+    b = 7
+    c = a + b
+    return c
+# return = panggil variable / fungsi sebagai nilai
+
+function_1 = function_x() + 5
+print(function_1)
