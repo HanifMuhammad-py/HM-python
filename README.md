fc6a38e72fee77369306f2f5acf5292c3a12ae9e
HanifMuhammad-py <hanifmhmmd13@gmail.com>
2026-06-13 07:13:47 +0700

    Python Basic

--- README.md
+++
@@ -1,3 +0,0 @@
-# HM-python
-Repository For Learning and Trying Basic Python.
-0 to 1
--- __init__.py
+++
@@ -1,7 +0,0 @@
-# dengan memasukan file __init__ ke dalam suatu struktur folder,
-# maka semua file dalam folder, bahkan folder itu sendiri, 
-# menjadi bisa di import dimanapun
-
-# __init__ difungsikan untuk IMPORT LINTAS FOLDER/PACKAGE
-
-from . import maintest
